f96ed783818f74ed3c8259d9afd990eae84faec0
Joshua Holloway <jhollow6@asu.edu>
2023-01-18 13:49:53 -0600

    loginApi() REST api endpoint for POST request to login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,56 +8,90 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    // ------------------------------------------
+  // ------------------------------------------
 
-    public function profile(User $user) {
+  public function profile(User $user) {
 
-      // $thePosts = $user->posts()->latest()->get();
-      // $postCount = $user->posts()->count();
-      $theOrders = $user->orders()->latest()->get();
-      $orderCount = $user->orders()->count();
+    // $thePosts = $user->posts()->latest()->get();
+    // $postCount = $user->posts()->count();
+    $theOrders = $user->orders()->latest()->get();
+    $orderCount = $user->orders()->count();
 
-      // RETURN THIS AND REFRESH PAGE!!!
-      // RETURN THIS AND REFRESH PAGE!!!
-      // RETURN THIS AND REFRESH PAGE!!!
-      // RETURN THIS AND REFRESH PAGE!!!
-      // RETURN THIS AND REFRESH PAGE!!!
-      // return $theOrders;
+    // RETURN THIS AND REFRESH PAGE!!!
+    // RETURN THIS AND REFRESH PAGE!!!
+    // RETURN THIS AND REFRESH PAGE!!!
+    // RETURN THIS AND REFRESH PAGE!!!
+    // RETURN THIS AND REFRESH PAGE!!!
+    // return $theOrders;
 
-      return view('profile-posts', [
-        // 'username' => $user->username, 
-        'email' => $user->email, 
-        // 'posts' => $thePosts, 
-        'posts' => $theOrders, 
-        // 'postCount' => $postCount,
-        'postCount' => $orderCount,
-      ]);
-      // return view('profile-posts', ['username' => $user->username]);
+    return view('profile-posts', [
+      // 'username' => $user->username, 
+      'email' => $user->email, 
+      // 'posts' => $thePosts, 
+      'posts' => $theOrders, 
+      // 'postCount' => $postCount,
+      'postCount' => $orderCount,
+    ]);
+    // return view('profile-posts', ['username' => $user->username]);
+  }
+
+  // ------------------------------------------
+
+  public function login(Request $request) {
+    $incoming_fields = $request->validate([
+      'loginusername' => 'required',
+      'loginpassword' => 'required',
+    ]);
+
+
+    return "/api/login";
+
+    // -Hash and compare
+    // if (auth()->attempt(['username' => $incoming_fields['loginusername'], 'password' => $incoming_fields['loginpassword']])) {
+    if (auth()->attempt([
+      'email' => $incoming_fields['loginusername'], 
+      'password' => $incoming_fields['loginpassword']]
+    )) {
+      
+      // Log user in if successful:
+      $request->session()->regenerate();
+      
+      return 'Congrats!!!';
+    } else {
+        return 'Sorry!!!';
     }
+  }
 
-    // ------------------------------------------
+  // ------------------------------------------
 
-    public function login(Request $request) {
-      $incoming_fields = $request->validate([
-        'loginusername' => 'required',
-        'loginpassword' => 'required',
-      ]);
+  public function loginApi(Request $request) {
+    // return "/api/login [POST]";
+    // return $request;
 
-      // -Hash and compare
-      // if (auth()->attempt(['username' => $incoming_fields['loginusername'], 'password' => $incoming_fields['loginpassword']])) {
-      if (auth()->attempt(['email' => $incoming_fields['loginusername'], 'password' => $incoming_fields['loginpassword']])) {
-        
-        // Log user in if successful:
-        $request->session()->regenerate();
-        
-        return 'Congrats!!!';
-      } else {
-          return 'Sorry!!!';
-      }
+    $incoming_fields = $request-> validate([
+      'email'    => 'required',
+      'password' => 'required', 
+    ]);
+    // return $incoming_fields;
+
+
+    $is_valid_login = auth()->attempt([
+      'email' => $incoming_fields['email'], 
+      'password' => $incoming_fields['password']]
+    );
+    // return $is_valid_login;
+
+    if ($is_valid_login) {
+      $user = User::where('email', $incoming_fields['email'])->first();
+      $token = $user->createToken('ourapptoken')->plainTextToken;
+      return $token;
+    } else {
+      return false;
     }
+  }
 
-    // ------------------------------------------
-  
+  // ------------------------------------------
+
   public function register(Request $request) {
 
     $incomingFields = $request->validate([
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -15,6 +15,6 @@ class Order extends Model
     ];
 
     public function user() {
-      return $this->belongsTo(a, b);
-    }
+      return $this->belongsTo(User::class, 'user_id');
+  }
 }
