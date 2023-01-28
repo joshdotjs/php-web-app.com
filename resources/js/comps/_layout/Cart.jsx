4749763ccdcb0715baee0292dc39d41669795f54
Joshua Holloway <jhollow6@asu.edu>
2023-01-28 12:33:09 -0600

    trash icon

--- resources/js/comps/_layout/Cart.jsx
+++ resources/js/comps/_layout/Cart.jsx
@@ -284,7 +284,6 @@ export default function Cart() {
             className={`line-item
               bg-green-400 mb-5 px-4 py-2
             `} 
-            onClick={() => remove(item)}
             style={{ 
               display: item.status === 'exiting' ? 'none' : 'grid'
             }}
@@ -293,6 +292,16 @@ export default function Cart() {
             <p>{item.product.title}</p>
             <p>{item.variant.size} {' '} {item.variant.color}</p>
             <p>Qty: {item.qty}</p>
+
+            <svg 
+              onClick={() => remove(item)}
+              className="bi bi-trash  cursor-pointer" 
+              xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
+              viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+              <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg>
+            
           </div>
         );
       })}
@@ -305,7 +314,8 @@ export default function Cart() {
           position: 'absolute',
           bottom: '350px',
           left: '50%',
-          transform: 'translateX(-50%)'
+          transform: 'translateX(-50%)',
+          width: '50%'
         }}
       >
         <Button
