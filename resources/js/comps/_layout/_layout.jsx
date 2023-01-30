b9a1ddc9f084dc686a87f4cacf85d360b17fb6b5
Joshua Holloway <jhollow6@asu.edu>
2023-01-29 20:56:34 -0600

    deploy

--- resources/js/comps/_layout/_layout.jsx
+++ resources/js/comps/_layout/_layout.jsx
@@ -28,10 +28,19 @@ export default function Layout({ children, name, restrict }) {
     console.log('blur_overlay: ', blur_overlay);
     console.log('header: ', header);
 
-    const duration = 0.5;
     const delay = 0.125;
-    gsap.to(page, { opacity: 1, scale: 1, duration, delay });
-    gsap.to(blur_overlay, { opacity: 0, duration, delay, onComplete: () => blur_overlay.remove() });
+    const tl = gsap.timeline();
+    tl.to(page, { opacity: 1, scale: 1, duration: 1.2, delay });
+    tl.to(blur_overlay, { 
+      backdropFilter: 'blur(0px)', 
+      // opacity: 0, 
+      duration: 0.5, 
+      delay, 
+      onComplete: () => blur_overlay.remove() 
+    }, 
+    '<='
+    // '-=0.25'
+    );
     // gsap.to(header, { opacity: 1, duration: 1 });
 
   }, []);
@@ -49,7 +58,7 @@ export default function Layout({ children, name, restrict }) {
 
         <Header { ...{ header_ref } } />
 
-        <main id="page" className={name} ref={page_ref} style={{ opacity: 0, transform: 'scale(1.005)', background: 'black' }}>
+        <main id="page" className={name} ref={page_ref} style={{ opacity: 0, transform: 'scale(1.02)', background: 'black' }}>
           {children}
         </main> 
 
@@ -57,18 +66,18 @@ export default function Layout({ children, name, restrict }) {
           id="blur-overlay" 
           ref={blur_ref}
           style={{ 
-          backdropFilter: 'blur(10px)', 
-          // background: 'rgba(0, 0, 0, 0.5)', 
-          // backdropFilter: 'blur(10px) saturate(180%)',
-          // '-webkit-backdrop-filter': 'blur(10px) saturate(180%)',
-          WebkitBackdropFilter: 'blur(10px) saturate(180%)',
-          // backgroundColor: 'rgba(17, 25, 40, 0.75)',
-          position: 'fixed', 
-          height: '100vh',
-          width: '100vw',
-          top: 0,
-          left: 0,
-          zIndex: 10,
+            backdropFilter: 'blur(10px)', 
+            // background: 'rgba(0, 0, 0, 0.5)', 
+            // backdropFilter: 'blur(10px) saturate(180%)',
+            // '-webkit-backdrop-filter': 'blur(10px) saturate(180%)',
+            WebkitBackdropFilter: 'blur(10px) saturate(180%)',
+            // backgroundColor: 'rgba(17, 25, 40, 0.75)',
+            position: 'fixed', 
+            height: '100vh',
+            width: '100vw',
+            top: 0,
+            left: 0,
+            zIndex: 10,
           }}
         ></div>
       
