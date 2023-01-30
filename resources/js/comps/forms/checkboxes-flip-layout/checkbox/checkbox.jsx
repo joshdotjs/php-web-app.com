11737d79c69c4b6969d15f062dc54195424961e4
Joshua Holloway <jhollow6@asu.edu>
2023-01-29 18:55:42 -0600

    filter checkboxes start checked

--- resources/js/comps/forms/checkboxes-flip-layout/checkbox/checkbox.jsx
+++ resources/js/comps/forms/checkboxes-flip-layout/checkbox/checkbox.jsx
@@ -8,31 +8,36 @@ export default function Checkbox({ id, setSet, option, applyFilter }) {
 
   // --------------------------------------------
 
+  // -This version differs from the main version 
+  //  by applying applyFilter(option) instead
+  //  of updating the setSet state.
+  // -applyFilter(option) is specific to the 
+  //  FLIP grid logic and the Set is updated
+  //  in-place in that function, as well as the
+  //  layout state which triggers the FLIP animation.
+  // -The goal of updating the state in place like this
+  //  was to reduce the number of useEffects to 
+  //  only one to avoid any unnecessary side effect complexity.
+  // -Really only useLayoutEffect is used
+  //  in response the the change of the layout
+  //  state property named 'state' which stores
+  //  the FLIP snapshot of the grid state.
+
+  // --------------------------------------------
+
   // -option:   the string option correponding to this checkbox
   // -setSet:   set the external state that stores the set of chosen checkboxes
   // -id:       unique identifier used in htmlFor
 
   // --------------------------------------------
 
-  const [state, setState] = useState(false);
+  const [state, setState] = useState(true);
 
   // --------------------------------------------
 
   const onChange = (event) => {
     const { checked, type, name, value } = event.target;
     setState(checked); // local state for 'controlled-input'
-
-    // setSet(prev => { // external state storing the set of chosen checkboxes
-    //   const clone = new Set([...prev]);
-
-    //   if (prev.has(option)) {
-    //     clone.delete(option);
-    //   }
-    //   else {
-    //     clone.add(option);
-    //   }
-    //   return clone;
-    // });
     applyFilter(option);
   };
 
