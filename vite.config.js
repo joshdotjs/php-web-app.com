07317b14ebc3890e79ff8fd14293a8669b1c207c
Joshua Holloway <jhollow6@asu.edu>
2023-01-31 14:12:54 -0600

    fuck that react-refresh package

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import reactRefresh from '@vitejs/plugin-react-refresh'; // NEW
+// import reactRefresh from '@vitejs/plugin-react-refresh'; // NEW
 import path from 'path';
 
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
           input: ['resources/css/app.css', 'resources/js/app.js'],
           refresh: true,
       }),
-      reactRefresh(), // NEW
+      // reactRefresh(), // NEW
   ],
   resolve: { // https://javascript.plainenglish.io/how-to-set-up-path-resolving-in-vite-ad284e0d9eae
     alias: {
