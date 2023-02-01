ad7e5e5b06a0b6bfafc899dff840896e0814f321
Joshua Holloway <jhollow6@asu.edu>
2023-01-31 19:24:15 -0600

    dropdown port

--- resources/js/pages/store/Grid.jsx
+++ resources/js/pages/store/Grid.jsx
@@ -1,10 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { gsap } from 'gsap';
 
 import Card from './card';
 
-import Dropdown1 from './dropdown/dropdown-1';
-import Dropdown2 from './dropdown/dropdown-2';
+import Dropdown from './dropdown/dropdown';
 
 // ==============================================
 
@@ -81,8 +80,6 @@ export default function Grid({
 
   const openFiltersHandler = () => {
 
-    console.log('fuck')
-
     setShowFilters((prev) => {
 
       if (prev) {
@@ -97,9 +94,9 @@ export default function Grid({
   }
 
   // --------------------------------------------
-  
-  const dropdown_options = ['one', 'two', 'three'];
-  
+
+  const [sort_type, setSortType] = useState({ title: '',  sub_title: '', type: '', }); // e.g. { title: 'Price',  sub_title: 'High-Low', type: 'DESC'  }
+
   // --------------------------------------------
 
   return (
@@ -112,12 +109,15 @@ export default function Grid({
           <div id="show-filters-button" onClick={openFiltersHandler}>
             { show_filters && <h5>Hide Filters</h5>}
             { !show_filters && <h5>Show Filters</h5>}
-            <svg aria-hidden="true" className="icon-filter-ds" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none"><path stroke="currentColor" strokeWidth="1.5" d="M21 8.25H10m-5.25 0H3"></path><path stroke="currentColor" strokeWidth="1.5" d="M7.5 6v0a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" clipRule="evenodd"></path><path stroke="currentColor" strokeWidth="1.5" d="M3 15.75h10.75m5 0H21"></path><path stroke="currentColor" strokeWidth="1.5" d="M16.5 13.5v0a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" clipRule="evenodd"></path></svg>
+            <svg aria-hidden="true" className="icon-filter-ds" focusable="false" viewBox="0 0 24 24" role="img" width="24px" height="24px" fill="none">
+              <path stroke="currentColor" strokeWidth="1.5" d="M21 8.25H10m-5.25 0H3"></path>
+              <path stroke="currentColor" strokeWidth="1.5" d="M7.5 6v0a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" clipRule="evenodd"></path>
+              <path stroke="currentColor" strokeWidth="1.5" d="M3 15.75h10.75m5 0H21"></path>
+              <path stroke="currentColor" strokeWidth="1.5" d="M16.5 13.5v0a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" clipRule="evenodd"></path>
+            </svg>
           </div>
 
-          <Dropdown1 />
-
-          {/* <Dropdown2 /> */}
+          <Dropdown { ...{ sort_type, setSortType } } />
         
         </div>
 
--- resources/js/pages/store/dropdown/dropdown.jsx
+++ resources/js/pages/store/dropdown/dropdown.jsx
@@ -5,24 +5,24 @@ import { disableClick, enableClick } from '../../../util/dom';
 // ==============================================
 
 const sort_options_arr = [
-  { title: 'Price',  sub_title: 'High to low', col: 'price',  type: 'DESC'  },
-  { title: 'Price',  sub_title: 'Low to high', col: 'price',  type: 'ASC' },
-  { title: 'Name',   sub_title: 'A to Z',      col: 'title',  type: 'ASC'  },
-  { title: 'Name',   sub_title: 'Z to A',      col: 'title',  type: 'DESC' },
-  { title: 'Rating', sub_title: '',            col: 'rating', type: 'DESC'  },
+  { title: 'Price',  sub_title: 'High-Low', type: 'DESC'  },
+  { title: 'Price',  sub_title: 'Low-High', type: 'ASC' },
+  { title: 'Name',   sub_title: 'A-Z',      type: 'ASC'  },
+  { title: 'Name',   sub_title: 'Z-A',      type: 'DESC' },
 ];
 
 // ==============================================
 
 export default function Dropdown({
-  updateProducts, setSort
+  // updateProducts, 
+  sort_type,
+  setSortType
 }) {
 
   // --------------------------------------------
 
   const chooseSort = (sort_type, idx) => {
 
-    
     disableClick(); // chooseSort() is run along with hideSortDropdown() => enableClick() run on completion of animation
 
     const duration = 0.1;
@@ -51,7 +51,7 @@ export default function Dropdown({
     });
 
     // update sort state: 
-    // setSort(sort_type);
+    setSortType(sort_type);
 
     // updateProducts({ sort_type });
   };
@@ -113,20 +113,13 @@ export default function Dropdown({
   // --------------------------------------------
 
   const toggleSortDropdown = () => { 
-    setShowSortDropdown(prev => {
-  
-      const curr = !prev;
-
-      // ${show_sort_dropdown ? 'translate-x-0 opacity-100 scale-100 z-10' : 'translate-x-0 transform opacity-0 scale-95'} ease-in-out duration-300
-
-      if (curr === true) {
-        showSortDropdown();
-        addEventListener('click', hideSortDropdown, { once: true });
-      } else {
-        hideSortDropdown();
-        removeEventListener('click', hideSortDropdown, { once: true });
-      }
-    }); 
+    if (show_sort_dropdown) {
+      hideSortDropdown();
+      removeEventListener('click', hideSortDropdown, { once: true });
+    } else {
+      showSortDropdown();
+      addEventListener('click', hideSortDropdown, { once: true });
+    }
   };
 
   // --------------------------------------------
@@ -145,7 +138,9 @@ export default function Dropdown({
           aria-expanded="false" 
           aria-haspopup="true"
         >
-          Sort
+          <span>Sort By{sort_type?.sub_title && ': '}</span>
+          { sort_type?.title && <strong className="ml-2">{sort_type?.title}: </strong> }
+          { sort_type?.sub_title && <span className="ml-2">{sort_type?.sub_title}</span> }
           {/* <!-- Heroicon name: mini/chevron-down --> */}
           <svg className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
@@ -176,44 +171,36 @@ export default function Dropdown({
         <div 
           className="py-1" role="none" onClick={e => e.stopPropagation()}
         >
-            {/* <!--
-              Active: "bg-gray-100", Not Active: ""
-
-              Selected: "font-medium text-gray-900", Not Selected: "text-gray-500"
-            --> */}
-
-
-            { sort_options_arr.map(({ title, sub_title, type, col }, idx) => (
-              <button 
-                key={`${title}-${idx}`}
-                onClick={() => chooseSort({ type, col }, idx)}
-                style={{ width: '200px' }}
-                className="text-left font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">
+          { sort_options_arr.map(({ title, sub_title, type }, idx) => (
+            <button 
+              key={`${title}-${idx}`}
+              onClick={() => chooseSort({ title, sub_title, type }, idx)}
+              style={{ width: '200px' }}
+              className="text-left font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0"
+            >
                   
-                  <svg //  Heroicon name: mini/check
-                    ref={el => check_refs.current[idx] = el}
-                    style={{ opacity: 0 }}
-                    className="inline text-indigo-600 texth-5 w-5 mr-2" 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    viewBox="0 0 20 20" 
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
-                  </svg>
+              <svg //  Heroicon name: mini/check
+                ref={el => check_refs.current[idx] = el}
+                style={{ opacity: 0 }}
+                className="inline text-indigo-600 texth-5 w-5 mr-2" 
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 20 20" 
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+              </svg>
 
-                  <strong>
-                    {title}{sub_title && ': '}
-                  </strong>
-                    {sub_title}
-              </button>
-            )) }
+              <strong>
+                {title}{sub_title && ': '}
+              </strong>
 
+              {sub_title}
 
-
+            </button>
+          ))}
         </div>
       </div>
-    
     </div>
   );
 
