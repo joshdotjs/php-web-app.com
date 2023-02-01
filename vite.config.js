a38fbc878d9da4523b129c6c59ebb648b409d39c
Joshua Holloway <jhollow6@asu.edu>
2023-01-31 18:31:52 -0600

    added react-refresh back in...

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
+import reactRefresh from '@vitejs/plugin-react-refresh'; // NEW
+
 
 export default defineConfig({
   plugins: [
@@ -8,6 +10,7 @@ export default defineConfig({
           input: ['resources/css/app.css', 'resources/js/app.js'],
           refresh: true,
       }),
+      reactRefresh(), // NEW
   ],
   resolve: { // https://javascript.plainenglish.io/how-to-set-up-path-resolving-in-vite-ad284e0d9eae
     alias: {
