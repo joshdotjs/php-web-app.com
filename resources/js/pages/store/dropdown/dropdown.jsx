41b69f58f944642f5e7809150b4f048fc6ed5fb3
Joshua Holloway <jhollow6@asu.edu>
2023-01-31 19:28:58 -0600

    sub title gray

--- resources/js/pages/store/dropdown/dropdown.jsx
+++ resources/js/pages/store/dropdown/dropdown.jsx
@@ -139,8 +139,8 @@ export default function Dropdown({
           aria-haspopup="true"
         >
           <span>Sort By{sort_type?.sub_title && ': '}</span>
-          { sort_type?.title && <strong className="ml-2">{sort_type?.title}: </strong> }
-          { sort_type?.sub_title && <span className="ml-2">{sort_type?.sub_title}</span> }
+          { sort_type?.title && <strong className="ml-2" style={{ color: '#757575'}}>{sort_type?.title}: </strong> }
+          { sort_type?.sub_title && <span className="ml-2" style={{ color: '#757575'}}>{sort_type?.sub_title}</span> }
           {/* <!-- Heroicon name: mini/chevron-down --> */}
           <svg className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
