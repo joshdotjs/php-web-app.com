19ae6996ba386740cec0f551d01de6ef56f6c4b0
Joshua Holloway <jhollow6@asu.edu>
2023-02-03 17:23:27 -0600

    middle case

--- resources/js/pages/store/pagination.jsx
+++ resources/js/pages/store/pagination.jsx
@@ -34,37 +34,47 @@ export default function Pagination({ num_products, page_num, updatePageNum }) {
   // --------------------------------------------
 
   const products_per_page = 6;
-  const num_pages = Math.ceil(num_products / products_per_page)
+  const num_pages = Math.ceil(num_products / products_per_page); // 1-based, page_num is 0-based
 
   // --------------------------------------------
 
   let arr = [];
 
   // Case 1: Less than 7 pages    display: 1,2,3,4,5,6
-  // Case 2: >=7 pages            display: 1,2,3,...,7    page_num: 1,2       case-A
-  //                                                                 2,3,4,...,7    page_num: 3         case-B
-  //                                                                 3,4,5,...,7    page_num: 4
-  //                                                                 1,...,5,6,7    page_num: 5,6,7     case-C
 
 
-  // Case 2: >=6 pages            display: 
-  //                                       1,2,3,4,    ...,9    page_num: 1,2,3         case-A                   page_num === 1 | 2 | 3
-  //                                       1,...,3,4,5,...,9    page_num: 4             case-B                   3 < page_num < num_pages-2
-  //                                       1,...,4,5,6,...,9    page_num: 5
-  //                                       1,...,5,6,7,...,9    page_num: 6
-  //                                       1,...,    6,7,8,9    page_num: 7,8,9         case-C                   page_num === num_pages-2 | num_pages-1 | num_pages
+  // Case 2:   num_pages: 7        pagination             page_num 
+  //                               1,2,3,4,    ...,7      1,2,3         case-A
+  //                               1,...,3,4,5,...,7      4             case-B
+  //                               1,...,    4,5,6,7      5,6,7         case-C
 
-  if (num_pages < 7) { // case 1
+  // Case 2:   num_pages: 8        pagination             page_num 
+  //                               1,2,3,4,    ...,8      1,2,3         case-A
+  //                               1,...,3,4,5,...,8      4             case-B
+  //                               1,...,4,5,6,...,8      5
+  //                               1,...,    5,6,7,8      6,7,8         case-C
+
+  // Case 2:   num_pages: 9        pagination             page_num
+  //                               1,2,3,4,    ...,9      1,2,3         case-A                   page_num === 1 | 2 | 3
+  //                               1,...,3,4,5,...,9      4             case-B                   3 < page_num < num_pages-2=7
+  //                               1,...,4,5,6,...,9      5
+  //                               1,...,5,6,7,...,9      6
+  //                               1,...,    6,7,8,9      7,8,9         case-C                   page_num === num_pages-2=7 | num_pages-1=8 | num_pages=9
+
+  if (num_pages < 7) { // case 1 (num_pages < 7)
     for (let i = 0; i < num_pages; ++i) {
       arr.push(<Fragment key={`pagination-link-${i + 1}`}><Link {...{page_num, updatePageNum}}>{i + 1}</Link></Fragment>);
     }
-  } else if (num_pages >= 7) { // case 2
+  } else { // case 2 (num_pages >= 7)
 
-    // if (page_num === 1 || page_num === 2) { // case-A
+    console.clear();
+    console.log('page_num: ', page_num)
+
+    if (page_num < 3) { // case-A
       arr.push(<Fragment key={`pagination-link-${0 + 1}`}><Link {...{page_num, updatePageNum}}>{0 + 1}</Link></Fragment>);
       arr.push(<Fragment key={`pagination-link-${1 + 1}`}><Link {...{page_num, updatePageNum}}>{1 + 1}</Link></Fragment>);
       arr.push(<Fragment key={`pagination-link-${2 + 1}`}><Link {...{page_num, updatePageNum}}>{2 + 1}</Link></Fragment>);
-      
+      arr.push(<Fragment key={`pagination-link-${3 + 1}`}><Link {...{page_num, updatePageNum}}>{3 + 1}</Link></Fragment>);       
       arr.push(
         <Fragment key={`pagination-link-${3 + 1}`}>
           <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
@@ -72,17 +82,32 @@ export default function Pagination({ num_products, page_num, updatePageNum }) {
           </span>
         </Fragment>
       );
-              
-      arr.push(<Fragment key={`pagination-link-${4 + 1}`}><Link {...{page_num, updatePageNum}}>{num_pages}</Link></Fragment>);
+      arr.push(<Fragment key={`pagination-link-${num_pages}`}><Link {...{page_num, updatePageNum}}>{num_pages}</Link></Fragment>);
 
-    // } else if (page_num <= num_pages - 3) { // case-B
-    // } else { // case-C
-    // }
-    
+    } else if (page_num <= num_pages - 2) { // case-B
 
+      arr.push(<Fragment key={`pagination-link-${0 + 1}`}><Link {...{page_num, updatePageNum}}>{0 + 1}</Link></Fragment>);
+      arr.push(
+        <Fragment key={`pagination-link-${1 + 1}`}>
+          <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
+            ...
+          </span>
+        </Fragment>
+      );
+      arr.push(<Fragment key={`pagination-link-${(page_num-1) + 1}`}><Link {...{page_num, updatePageNum}}>{(page_num-1) + 1}</Link></Fragment>);
+      arr.push(<Fragment key={`pagination-link-${(page_num)   + 1}`}><Link {...{page_num, updatePageNum}}>{(page_num)   + 1}</Link></Fragment>);
+      arr.push(<Fragment key={`pagination-link-${(page_num+1) + 1}`}><Link {...{page_num, updatePageNum}}>{(page_num+1) + 1}</Link></Fragment>);
+      arr.push(
+        <Fragment key={`pagination-link-${num_pages-1}`}>
+          <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
+            ...
+          </span>
+        </Fragment>
+      );
+      arr.push(<Fragment key={`pagination-link-${num_pages}`}><Link {...{page_num, updatePageNum}}>{num_pages}</Link></Fragment>);
 
-
-
+    } else { // case-C
+    }
   }
 
 
