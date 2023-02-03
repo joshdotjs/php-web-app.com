11a6b7b958ed40e115c9051c73c3e47d5bd8a015
Joshua Holloway <jhollow6@asu.edu>
2023-02-03 17:37:11 -0600

    disable buttons (mobile) if at end of boundaries

--- resources/js/pages/store/pagination.jsx
+++ resources/js/pages/store/pagination.jsx
@@ -132,13 +132,17 @@ export default function Pagination({ num_products, page_num, updatePageNum }) {
       <div className="flex flex-1 justify-between sm:hidden">
         <a
           // href="#"
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          onClick={() => page_num > 0 && updatePageNum(page_num - 1)}
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 cursor-pointer"
+          style={page_num === 0 ? { opacity: 0.5, pointerEvents: 'none' } : {}}
         >
           Previous
         </a>
         <a
           href="#"
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          onClick={() => page_num < num_pages && updatePageNum(page_num + 1)}
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 cursor-pointer"
+          style={page_num === num_pages-1 ? { opacity: 0.5, pointerEvents: 'none' } : {}}
         >
           Next
         </a>
