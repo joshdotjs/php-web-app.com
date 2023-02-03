0c50131cf45404d18754a5da009d06c47fc09db8
Joshua Holloway <jhollow6@asu.edu>
2023-02-03 17:49:26 -0600

    desktop pagination arrows

--- resources/js/pages/store/pagination.jsx
+++ resources/js/pages/store/pagination.jsx
@@ -158,7 +158,9 @@ export default function Pagination({ num_products, page_num, updatePageNum }) {
           <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
             <a
               // href="#"
-              className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+              onClick={() => page_num > 0 && updatePageNum(page_num - 1)}
+              className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 cursor-pointer"
+              style={page_num === 0 ? { opacity: 0.5, pointerEvents: 'none' } : {}}
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
@@ -172,7 +174,9 @@ export default function Pagination({ num_products, page_num, updatePageNum }) {
 
             <a
               // href="#"
-              className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+              onClick={() => page_num < num_pages && updatePageNum(page_num + 1)}
+              className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 cursor-pointer"
+              style={page_num === num_pages-1 ? { opacity: 0.5, pointerEvents: 'none' } : {}}
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
