e8c76cd5c76b9e4b56b237cd2c5644b8973e5441
Joshua Holloway <jhollow6@asu.edu>
2023-02-04 16:40:12 -0600

    responsive gap

--- resources/js/comps/_layout/_layout.jsx
+++ resources/js/comps/_layout/_layout.jsx
@@ -3,6 +3,7 @@ import { gsap } from "gsap";
 import { AuthContextProvider } from "@/context/auth-ctx";
 import { CartContextProvider } from "@/context/cart-ctx";
 
+
 import Header from './Header';
 import Cart from './Cart';
 import Notifications from './notify/notify';
--- resources/js/pages/about/_about.jsx
+++ resources/js/pages/about/_about.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import uuid from 'react-uuid';
 
 
-const lis = [{id: uuid()},  {id: uuid()}];
+const lis = [
+  {id: uuid(), img: '/img/products/shoes/women/Pegasus-Trail-4-pink.webp'},  
+  {id: uuid(), img: '/img/products/shoes/women/Structure-24-white.webp'}, 
+  {id: uuid(), img: '/img/products/shoes/women/React-Infinity-3 By-You-white.jpeg'}
+];
 
 // ==============================================
 
@@ -13,37 +17,55 @@ export default function Page() {
   return(
     <div id="grid-container"> 
 
-      <div id="grid-left"></div>
+      {/* =================================== */}
+
+      <div id="grid-left">
+        {/* Grid Left */}
+      </div>
+
+      {/* =================================== */}
+
       <div id="grid-right">
 
-        <div>
-          <div id="filter-button-row">
-          </div>
+        {/* --------------------------------- */}
 
-          <ul id="grid-items">
-            {
-              lis.map((li) => {
-                return (
-                  <li key={li.id} className="box">
-                    <div className="box-child">
-                      <div className="card">
-                        <div className="img-container"> <img /> </div>
-                        <div className="card-bottom">
-                          <div className="front">CARD</div>
-                          <div className="back"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                );
-              })
-            }
-          </ul>
-
-          <div id="pagination"></div>
+        <div id="filter-button-row">
+          Filter button row
         </div>
 
+        {/* --------------------------------- */}
+
+        <ul id="grid-items">
+          {
+            lis.map((li) => {
+              return (
+                <li key={li.id} className="box">
+                  <div className="box-child">
+                    <div className="card">
+                      <div className="img-container"> <img src={li.img}/> </div>
+                      <div className="card-bottom">
+                        <div className="front">CARD</div>
+                        <div className="back"></div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              );
+            })
+          }
+        </ul>
+
+        {/* --------------------------------- */}
+
+        <div id="pagination">
+          Pagination row
+        </div>
+
+        {/* --------------------------------- */}
+
       </div>
+
+      {/* =================================== */}
 
     </div>
   );
