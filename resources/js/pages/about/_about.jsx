8034f406bea5a7256e47482be564f69136c4cb3b
Joshua Holloway <jhollow6@asu.edu>
2023-02-04 14:51:30 -0600

    header styling

--- resources/js/pages/about/_about.jsx
+++ resources/js/pages/about/_about.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import uuid from 'react-uuid';
+
+
+const lis = [{id: uuid()},  {id: uuid()}];
 
 // ==============================================
 
@@ -7,32 +11,41 @@ export default function Page() {
   // --------------------------------------------
 
   return(
-    <>  
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-      <h1>About Page</h1>
-    </>
+    <div id="grid-container"> 
+
+      <div id="grid-left"></div>
+      <div id="grid-right">
+
+        <div>
+          <div id="filter-button-row">
+          </div>
+
+          <ul id="grid-items">
+            {
+              lis.map((li) => {
+                return (
+                  <li key={li.id} className="box">
+                    <div className="box-child">
+                      <div className="card">
+                        <div className="img-container"> <img /> </div>
+                        <div className="card-bottom">
+                          <div className="front">CARD</div>
+                          <div className="back"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                );
+              })
+            }
+          </ul>
+
+          <div id="pagination"></div>
+        </div>
+
+      </div>
+
+    </div>
   );
 
   // --------------------------------------------
