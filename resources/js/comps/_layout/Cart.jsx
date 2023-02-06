e09ae9c8552a1b1f55d8b77db6f6d2c949c5c508
Joshua Holloway <jhollow6@asu.edu>
2023-02-05 21:12:44 -0600

    blur overlay

--- resources/js/comps/_layout/Cart.jsx
+++ resources/js/comps/_layout/Cart.jsx
@@ -1,9 +1,10 @@
-import React, { Fragment, useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { Fragment, useCallback, useEffect, useLayoutEffect, useRef, useState, useContext } from 'react';
 import { createPortal } from 'react-dom';
 import uuid from 'react-uuid';
 import { gsap } from "gsap";
 import { Flip } from "gsap/Flip";
 
+import CartContext from '@/context/cart-ctx';
 
 import Button from '@/comps/button/button';
 
@@ -130,8 +131,10 @@ export default function Cart() {
 
   openCart = ({ onComplete=null }) => {
 
-    console.log('openCart()');
+    openDrawerCart();
 
+    console.log('openCart()');
+    
     const container = container_ref?.current;
 
     lr(tl_ref.current);
@@ -323,16 +326,57 @@ export default function Cart() {
   }, [ctx, layout, q, removeItems]);
 
   // --------------------------------------------
+
+  const { overlay_ref } = useContext(CartContext);
+
+  // --------------------------------------------
+  
+  const openDrawerCart = () => {
+    // setDrawerCartOpen(true);
+
+    console.log('openDrawerCart()');
+
+    const container = document.querySelector('#portal-cart');
+    container.style.zIndex = 100;
+    // console.log('container: ',  container);
+
+    lr('opening cart drawer');
+    const ref = overlay_ref.current;
+    ref.style.display = 'block';
+    gsap.to(ref, { 
+      opacity: 1, 
+      duration: 0.3,
+      onStart: () => {
+        document.body.style.overflow = "hidden"; // don't scroll stuff underneath the modal
+      },
+    });
+  };
+
+  // --------------------------------------------
   
   return createPortal(
-    <aside 
-      id="cart" 
-      ref={container_ref}
-      className="
-        w-[300px]
-        md:w-[350px]
-      " 
-      style={{ position: 'fixed',
+    <>
+      <div // overlay
+        ref={overlay_ref}
+        className="pointer-events-auto fixed inset-0"
+        style={{ 
+          display: 'none', 
+          opacity: 0,
+          background: 'rgba(0, 0, 0, 0.8)',
+          backdropFilter: 'blur(5px)', // Hey - I think this is not animating the blur!  I think a single blur is computed and then the opacity on it is animated - which is efficient.  I think animating a blur causes a diffrent blur to be computed for each frame of the animation with each one slightly more blurred than the previous.
+        }}
+      >  
+      </div>
+
+      <aside 
+        id="cart" 
+        ref={container_ref}
+        className="
+          w-[300px]
+          md:w-[350px]
+          z-100
+        " 
+        style={{ position: 'fixed',
         top: 0,
         right: 0,
         background: 'white',
@@ -341,200 +385,202 @@ export default function Cart() {
         zIndex: 100,
         transform: 'translate(100%)'
       }}
-    >
-
-      {/* - - - - - - - - - - - - - - - - - - */}
-
-      <div
-        className="ml-4 mr-6 pt-8 pb-4"
-        style={{ 
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          // background: 'lightgreen'
-        }}
       >
 
-        <h4>Shopping Cart</h4>
+        {/* - - - - - - - - - - - - - - - - - - */}
 
-        <svg onClick={closeCart}
-          xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" 
-          className="bi bi-x  cursor-pointer" viewBox="0 0 16 16"
-          // style={{ background: 'red' }}
-        >
-          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-        </svg>
-      </div>
+        <div
+          className="ml-4 mr-6 pt-8 pb-4"
+          style={{ 
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            // background: 'lightgreen'
+          }}
+          >
 
-      
-      {/* - - - - - - - - - - - - - - - - - - */}
+          <h4>Shopping Cart</h4>
 
-      {layout.items.map((item, idx) => {
-        console.log('item: ', item);
+          <svg onClick={closeCart}
+            xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" 
+            className="bi bi-x  cursor-pointer" viewBox="0 0 16 16"
+            // style={{ background: 'red' }}
+            >
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+          </svg>
+        </div>
 
-        const {status, product: { title, sub_title, body, price, category }, variant: { id, img, color, size, qty }} = item;
+        
+        {/* - - - - - - - - - - - - - - - - - - */}
 
-        const key = `line-item-${id}`;
-
-        return (
-          <div  
+        {layout.items.map((item, idx) => {
+          console.log('item: ', item);
+          
+          const {status, product: { title, sub_title, body, price, category }, variant: { id, img, color, size, qty }} = item;
+          
+          const key = `line-item-${id}`;
+          
+          return (
+            <div  
             id={key} 
             key={key}
             data-flip-id={key}
             className={`line-item
-              ml-4 mr-6 py-4
-              ${idx !== layout.items.length - 1 ? 'border-b border-gray-200' : ''}
+            ml-4 mr-6 py-4
+            ${idx !== layout.items.length - 1 ? 'border-b border-gray-200' : ''}
             `}
             style={{ 
               display: status === 'exiting' ? 'none' : 'flex',
               // padding: '1rem',
               // outline: 'dashed yellow 1px',
             }}
-          >
-            <img 
-              className="rounded-md border border-gray-200"
-              src={img} 
-              style={{ 
-                height: '75px', 
-                // width: '75px' 
-              }} 
-            />
-
-            <div
-              style={{
-                // background: 'rgba(255, 0, 0, 0.2)',
-                flexGrow: 1,
-                // outline: 'dashed hotpink 2px',
-                display: 'flex',
-                flexDirection: 'column',
-                marginLeft: '1rem'
-              }}
             >
-              <div 
-                // className="mb-1"
+              <img 
+                className="rounded-md border border-gray-200"
+                src={img} 
+                style={{ 
+                  height: '75px', 
+                  // width: '75px' 
+                }} 
+                />
+
+              <div
                 style={{
+                  // background: 'rgba(255, 0, 0, 0.2)',
+                  flexGrow: 1,
+                  // outline: 'dashed hotpink 2px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginLeft: '1rem'
+                }}
+                >
+                <div 
+                  // className="mb-1"
+                  style={{
+                    // background: 'yellow',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    // marginBottom: '0.1rem'
+                  }}>
+                  <Ellipsis color='black' fontSize='0.9rem' fontWeight='500' classes="text-left">{title}</Ellipsis>
+                  <p 
+                    className=""
+                    style={{
+                      // background: 'red',
+                      fontSize: '0.9rem'
+                    }}
+                    >
+                    ${price / 100}
+                  </p>
+                </div>
+
+                <Ellipsis color={light} fontSize='0.8rem' fontWeight='400' classes="mb-2">{sub_title}</Ellipsis>
+                  
+                <div style={{
                   // background: 'yellow',
                   display: 'flex',
                   justifyContent: 'space-between',
-                  alignItems: 'center',
-                  // marginBottom: '0.1rem'
-              }}>
-                <Ellipsis color='black' fontSize='0.9rem' fontWeight='500' classes="text-left">{title}</Ellipsis>
-                <p 
-                  className=""
-                  style={{
-                    // background: 'red',
-                    fontSize: '0.9rem'
-                  }}
-                >
-                  ${price / 100}
-                </p>
-              </div>
+                  // alignItems: 'center',
+                  // background: 'yellow',
+                  marginTop: 'auto'
+                }}>
+                  
+                  {/* <Ellipsis color='black' fontSize='0.8rem' fontWeight='400' classes="mt-3 mb-1 text-left">Qty: {item.qty}</Ellipsis> */}
+                  <p style={{ fontSize: '0.8rem' }}>Qty: {item.qty}</p>
 
-              <Ellipsis color={light} fontSize='0.8rem' fontWeight='400' classes="mb-2">{sub_title}</Ellipsis>
+                  <svg 
+                    onClick={() => remove(item)}
+                    className="bi bi-trash  cursor-pointer" 
+                    xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
+                    // fill="currentColor" 
+                    fill="black"
+                    viewBox="0 0 16 16"
+                    >
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                  </svg>
+                </div>
+
                 
-              <div style={{
-                // background: 'yellow',
-                display: 'flex',
-                justifyContent: 'space-between',
-                // alignItems: 'center',
-                // background: 'yellow',
-                marginTop: 'auto'
-              }}>
-                
-                {/* <Ellipsis color='black' fontSize='0.8rem' fontWeight='400' classes="mt-3 mb-1 text-left">Qty: {item.qty}</Ellipsis> */}
-                <p style={{ fontSize: '0.8rem' }}>Qty: {item.qty}</p>
+              </div>                          
+            </div>
+          );
+        })}
 
-                <svg 
-                  onClick={() => remove(item)}
-                  className="bi bi-trash  cursor-pointer" 
-                  xmlns="http://www.w3.org/2000/svg" width="16" height="16" 
-                  // fill="currentColor" 
-                  fill="black"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                  <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                </svg>
-              </div>
+        {/* - - - - - - - - - - - - - - - - - - */}
 
-              
-            </div>                          
-          </div>
-        );
-      })}
-
-      {/* - - - - - - - - - - - - - - - - - - */}
-
-      <div 
-        id="cart-btn-container"
-        className="border-t border-gray-200"
-        style={{ 
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          width: '100%',
-          paddingTop: '2rem',
-          paddingBottom: '2rem',
-          paddingLeft: '1rem',
-          paddingRight: '1.5rem',
-        }}
-      >
-
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            marginBottom: '0.125rem'
+        <div 
+          id="cart-btn-container"
+          className="border-t border-gray-200"
+          style={{ 
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            paddingTop: '2rem',
+            paddingBottom: '2rem',
+            paddingLeft: '1rem',
+            paddingRight: '1.5rem',
           }}
-        >
-          <h4>Subtotal</h4>
-          <h4>${layout.items.reduce((accumulator, currentValue) => accumulator + currentValue.product.price, 0) / 100}</h4>
-        </div>
-
-        <div style={{ marginBottom: '2rem' }}>
-          <p style={{ fontSize: '0.8rem', color: light, fontWeight: '400'}}>Shipping and taxes calculated at checkout.</p>
-        </div>
-
-        <Button
-          disabled={layout.items.length === 0}
-          onClick={() => { 
-            layout.items.map(item => remove(item));
-            setTimeout(closeCart, 200);
-          }}
-          classes="mb-4"
-        >
-          Clear
-        </Button>
-
-
-        <Button
-          disabled={layout.items.length === 0}
-          onClick={() => {
-            submit();
-          }}
-        >
-          Checkout
-        </Button>
-
-
-
-        <div className="text-center">
-          <button 
-            type="button" 
-            className="font-medium text-indigo-600 hover:text-indigo-500 mt-8"
-            onClick={() => closeCart()}
           >
-            Continue Shopping
-            <span aria-hidden="true"> &rarr;</span>
-          </button>
+
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: '0.125rem'
+            }}
+            >
+            <h4>Subtotal</h4>
+            <h4>${layout.items.reduce((accumulator, currentValue) => accumulator + currentValue.product.price, 0) / 100}</h4>
+          </div>
+
+          <div style={{ marginBottom: '2rem' }}>
+            <p style={{ fontSize: '0.8rem', color: light, fontWeight: '400'}}>Shipping and taxes calculated at checkout.</p>
+          </div>
+
+          <Button
+            disabled={layout.items.length === 0}
+            onClick={() => { 
+              layout.items.map(item => remove(item));
+              setTimeout(closeCart, 200);
+            }}
+            classes="mb-4"
+            >
+            Clear
+          </Button>
+
+
+          <Button
+            disabled={layout.items.length === 0}
+            onClick={() => {
+              submit();
+            }}
+            >
+            Checkout
+          </Button>
+
+
+
+          <div className="text-center">
+            <button 
+              type="button" 
+              className="font-medium text-indigo-600 hover:text-indigo-500 mt-8"
+              onClick={() => closeCart()}
+              >
+              Continue Shopping
+              <span aria-hidden="true"> &rarr;</span>
+            </button>
+          </div>
+
         </div>
 
-      </div>
+        {/* - - - - - - - - - - - - - - - - - - */}
 
-      {/* - - - - - - - - - - - - - - - - - - */}
-
-    </aside>,
+      </aside>
+    </>
+    ,
     portal_root
   );
 
--- resources/js/context/cart-ctx/index.jsx
+++ resources/js/context/cart-ctx/index.jsx
@@ -25,6 +25,8 @@ const CartContextProvider = ({ children }) => {
   const cart_btn_ref = useRef(null); // cartBtn = cart.querySelector(".btn-cart");
   const cart_icon_target_ref = useRef(null); // cartItems = cart.querySelector(".items");
   const cart_count_ref = useRef(null); // cartCount = cart.querySelector(".count");
+  
+  const overlay_ref = useRef(null);
 
   // --------------------------------------------
 
@@ -35,6 +37,7 @@ const CartContextProvider = ({ children }) => {
     cart_icon_target_ref,
     cart_count_ref,
     // cart_open,
+    overlay_ref,
   };
 
 
