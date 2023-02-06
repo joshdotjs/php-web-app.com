6defb2dd1f7ade3d95b0dfd3e23a66405b6dc93a
Joshua Holloway <jhollow6@asu.edu>
2023-02-05 22:34:19 -0600

    deploy

--- resources/js/comps/_layout/Cart.jsx
+++ resources/js/comps/_layout/Cart.jsx
@@ -415,7 +415,9 @@ export default function Cart() {
           display: 'none', 
           opacity: 0,
           background: 'rgba(0, 0, 0, 0.8)',
-          backdropFilter: 'blur(5px)', // Hey - I think this is not animating the blur!  I think a single blur is computed and then the opacity on it is animated - which is efficient.  I think animating a blur causes a diffrent blur to be computed for each frame of the animation with each one slightly more blurred than the previous.
+          backdropFilter: 'blur(5px)', // I think this is not animating the blur!  I think a single blur is computed and then the opacity on it is animated - which is efficient.  I think animating a blur causes a diffrent blur to be computed for each frame of the animation with each one slightly more blurred than the previous.
+          WebkitBackdropFilter: 'blur(5px)',
+
         }}
         onClick={() => closeCart()}
       >  
