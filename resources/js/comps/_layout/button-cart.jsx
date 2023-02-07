db590b46e59259b5fbe778795d2b13ba2e1c9ab6
Joshua Holloway <jhollow6@asu.edu>
2023-02-07 15:49:30 -0600

    deploy

--- resources/js/comps/_layout/button-cart.jsx
+++ resources/js/comps/_layout/button-cart.jsx
@@ -21,7 +21,7 @@ export default function CartButton({cart_btn_ref, cart_count_ref, cart_icon_targ
         <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
       </svg>
 
-      <div ref={cart_count_ref} id="cart-count" style={{ opacity: 0 }}>
+      <div ref={cart_count_ref} id="cart-count" style={{ opacity: 0, display: 'inline-block', textAlign:'left' }}>
         <span style={{ color: 'white' }} ></span>
       </div>
       
