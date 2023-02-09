a91107b604ebe10fe807b152e0d5dfe73f0a4a41
Joshua Holloway <jhollow6@asu.edu>
2023-02-08 21:43:33 -0600

    deploy

--- resources/js/pages/store/_store.jsx
+++ resources/js/pages/store/_store.jsx
@@ -489,7 +489,7 @@ export default function Page({ products_SSR, num_products_SSR }) {
 
       } // if (filters_ls)
     }; // cosnt initializeFiltersFromLS = () => {};
-    setTimeout(initializeFiltersFromLS, 200);
+    setTimeout(initializeFiltersFromLS, 50);
   }, []);
 
   // --------------------------------------------
