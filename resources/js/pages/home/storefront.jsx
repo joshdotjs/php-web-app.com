c8f53f66093931d7da4e9f6091793c095866edc1
joshdotjs <joshdotjs@gmail.com>
2023-02-17 13:23:41 -0600

    text on homepage

--- resources/js/pages/home/storefront.jsx
+++ resources/js/pages/home/storefront.jsx
@@ -56,7 +56,7 @@ export default function Storefront() {
       {/* <!-- Collection section --> */}
       <section aria-labelledby="collection-heading" className="mx-auto max-w-xl px-4 pt-24 sm:px-6 sm:pt-32 lg:max-w-7xl lg:px-8">
         <h2 id="collection-heading" className="text-2xl font-bold tracking-tight text-gray-900">Shop by Collection</h2>
-        <p className="mt-4 text-base text-gray-500">Each season, we collaborate with world-class designers to create a collection inspired by the natural world.</p>
+        <p className="mt-4 text-base text-gray-500">Click one of the links below to be sent to the store page with the corresponding filters applied.</p>
   
         <div className="mt-10 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-8 lg:space-y-0">
           <a href="#" className="group block">
@@ -64,8 +64,8 @@ export default function Storefront() {
               {/* <img src={`${PHP.site_url}/wp-content/uploads/2023/01/new-arrivals-mens.webp`} alt="Brown leather key ring with brass metal loops and rivets on wood table." className="h-full w-full object-cover object-center" /> */}
               <img src={`img/new-arrivals-mens.webp`} alt="Brown leather key ring with brass metal loops and rivets on wood table." className="h-full w-full object-cover object-center" />
             </div>
-            <h3 className="mt-4 text-base font-semibold text-gray-900">Handcrafted Collection</h3>
-            <p className="mt-2 text-sm text-gray-500">Keep your phone, keys, and wallet together, so you can lose everything at once.</p>
+            <h3 className="mt-4 text-base font-semibold text-gray-900">Men's Collection</h3>
+            <p className="mt-2 text-sm text-gray-500">The men's collection contains products that were made for men. There are several different sub-categories in the men's collection.</p>
           </a>
   
           <a href="#" className="group block">
@@ -73,8 +73,8 @@ export default function Storefront() {
               {/* <img src={`${PHP.site_url}/wp-content/uploads/2023/01/new-arrivals-women.webp`}  alt="Natural leather mouse pad on white desk next to porcelain mug and keyboard." className="h-full w-full object-cover object-center" /> */}
               <img src={`img/new-arrivals-women.webp`}  alt="Natural leather mouse pad on white desk next to porcelain mug and keyboard." className="h-full w-full object-cover object-center" />
             </div>
-            <h3 className="mt-4 text-base font-semibold text-gray-900">Organized Desk Collection</h3>
-            <p className="mt-2 text-sm text-gray-500">The rest of the house will still be a mess, but your desk will look great.</p>
+            <h3 className="mt-4 text-base font-semibold text-gray-900">Women's Collection</h3>
+            <p className="mt-2 text-sm text-gray-500">The wommen's collection contains products that were made for women. There are several different sub-categories in this collection.</p>
           </a>
   
           <a href="#" className="group block">
@@ -82,8 +82,8 @@ export default function Storefront() {
               {/* <img src={`${PHP.site_url}/wp-content/uploads/2023/01/new-arrivals-kids.webp`} alt="Person placing task list card into walnut card holder next to felt carrying case on leather desk pad." className="h-full w-full object-cover object-center" /> */}
               <img src={`img/new-arrivals-kids.webp`} alt="Person placing task list card into walnut card holder next to felt carrying case on leather desk pad." className="h-full w-full object-cover object-center" />
             </div>
-            <h3 className="mt-4 text-base font-semibold text-gray-900">Focus Collection</h3>
-            <p className="mt-2 text-sm text-gray-500">Be more productive than enterprise project managers with a single piece of paper.</p>
+            <h3 className="mt-4 text-base font-semibold text-gray-900">Kid's Collection</h3>
+            <p className="mt-2 text-sm text-gray-500">The kid's collection contains products that were made for kids. There are several different sub-categories in the kid's collection.</p>
           </a>
         </div>
       </section>
