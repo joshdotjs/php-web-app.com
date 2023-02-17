7cfb0ab9be489de582d08afe3d36c8265b25c76e
joshdotjs <joshdotjs@gmail.com>
2023-02-17 14:06:51 -0600

    deploy

--- resources/js/pages/home/storefront.jsx
+++ resources/js/pages/home/storefront.jsx
@@ -97,10 +97,10 @@ export default function Storefront() {
           <div className="relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
             <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
               <h2 id="social-impact-heading" className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                <span className="block sm:inline">TEXT</span>
+                <span className="block sm:inline">Free standard shipping</span>
               </h2>
-              <p className="mt-3 text-xl text-white">SUB-TEXT</p>
-              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Shop Women's Collection</a>
+              <p className="mt-3 text-xl text-white">All items in the store include free shipping and handling. Expedited shipping speeds are also available at a slightly increased cost. We even offer next day shipping!</p>
+              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Get Free Shipping</a>
             </div>
           </div>
         </div>
@@ -195,9 +195,9 @@ export default function Storefront() {
           </div>
           <div className="relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
             <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
-              <h2 id="comfort-heading" className="text-3xl font-bold tracking-tight text-white sm:text-4xl">TEXT</h2>
-              <p className="mt-3 text-xl text-white">SUB-TEXT</p>
-              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Shop Men's Collection</a>
+              <h2 id="comfort-heading" className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Buy now, pay later</h2>
+              <p className="mt-3 text-xl text-white">Get your products immediately and break the cost up into four payments spaced over a range of time.  Simply select "buy now, pay later" at checkout to take advantage!</p>
+              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Buy Now Pay Later</a>
             </div>
           </div>
         </div>
