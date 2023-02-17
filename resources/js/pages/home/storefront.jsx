7aefc74cedd6f324f11eeee0337be298c07fe941
joshdotjs <joshdotjs@gmail.com>
2023-02-17 13:27:00 -0600

    deploy

--- resources/js/pages/home/storefront.jsx
+++ resources/js/pages/home/storefront.jsx
@@ -97,11 +97,10 @@ export default function Storefront() {
           <div className="relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
             <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
               <h2 id="social-impact-heading" className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                <span className="block sm:inline">Level up</span>
-                <span className="block sm:inline">your desk</span>
+                <span className="block sm:inline">TEXT</span>
               </h2>
-              <p className="mt-3 text-xl text-white">Make your desk beautiful and organized. Post a picture to social media and watch it get more likes than life-changing announcements. Reflect on the shallow nature of existence. At least you have a really nice desk setup.</p>
-              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Shop Workspace</a>
+              <p className="mt-3 text-xl text-white">SUB-TEXT</p>
+              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Shop Women's Collection</a>
             </div>
           </div>
         </div>
@@ -196,9 +195,9 @@ export default function Storefront() {
           </div>
           <div className="relative bg-gray-900 bg-opacity-75 py-32 px-6 sm:py-40 sm:px-12 lg:px-16">
             <div className="relative mx-auto flex max-w-3xl flex-col items-center text-center">
-              <h2 id="comfort-heading" className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Simple productivity</h2>
-              <p className="mt-3 text-xl text-white">Endless tasks, limited hours, a single piece of paper. Not really a haiku, but we're doing our best here. No kanban boards, burndown charts, or tangled flowcharts with our Focus system. Just the undeniable urge to fill empty circles.</p>
-              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Shop Focus</a>
+              <h2 id="comfort-heading" className="text-3xl font-bold tracking-tight text-white sm:text-4xl">TEXT</h2>
+              <p className="mt-3 text-xl text-white">SUB-TEXT</p>
+              <a href="#" className="mt-8 block w-full rounded-md border border-transparent bg-white py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100 sm:w-auto">Shop Men's Collection</a>
             </div>
           </div>
         </div>
